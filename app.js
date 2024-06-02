af62b9a633482e0eab96c04043c66fdfc1ad47a9
Nik <nik.piao26@gmail.com>
2024-06-01 22:42:36 -0400

    fix: media queries styles, add: exports folder with quotes.py and json files

--- app.js
+++ app.js
@@ -6,26 +6,64 @@ const closeIcon = document.querySelector('#closeMenu');
 const openIcon = document.querySelector('#openMenu');
 
 function toggleMenu() {
-	if (menu.classList.contains('showMenu')) {
-		menu.classList.remove('showMenu');
-		closeIcon.style.display = 'none';
-		openIcon.style.display = 'block';
-	} else {
-		menu.classList.add('showMenu');
-		closeIcon.style.display = 'block';
-		openIcon.style.display = 'none';
-	}
+    if (menu.classList.contains('showMenu')) {
+        menu.classList.remove('showMenu');
+        closeIcon.style.display = 'none';
+        openIcon.style.display = 'block';
+    } else {
+        menu.classList.add('showMenu');
+        closeIcon.style.display = 'block';
+        openIcon.style.display = 'none';
+    }
 }
 
 menuBtn.addEventListener('click', toggleMenu);
 
 menuItems.forEach(menuItem => {
-	menuItem.addEventListener('click', toggleMenu);
+    menuItem.addEventListener('click', toggleMenu);
 });
 
+// Display random quotes by author using fetch
+// Load the JSON file with quotes
+// Load the JSON file with quotes
+fetch('exports/quotes.json')
+    .then(response => {
+        if (!response.ok) {
+            console.error('Network response was not ok');
+            return;
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data); // Logging the retrieved JSON data for debugging
+
+        // Store quotes data
+        const quoteData = data;
+
+        // Function to generate random quote
+        function generateQuote() {
+            // Select a random quote and author
+            const randomIndex = Math.floor(Math.random() * data.length);
+            const randomQuote = quoteData[randomIndex];
+
+            // Display the quote and author
+            const quoteElem = document.querySelector('#message');
+            const authorElem = document.querySelector('#author');
+
+            quoteElem.textContent = `"${randomQuote.quotes}"`;
+            authorElem.textContent = '- ' + randomQuote.author;
+        }
+
+        // Call the function initially to display a random quote
+        generateQuote();
+
+        // Set interval to generate new quote every 6 seconds
+        setInterval(generateQuote, 4000);
+    })
+    .catch(error => console.error('Error fetching quotes:', error));
 
 // Rotating text animation
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const texts = document.querySelectorAll('.text');
     let currentIdx = 0;
 
